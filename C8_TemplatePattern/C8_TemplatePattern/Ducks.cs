3790ebb1370839ab611f83dd576b4437c16f6c3c
Weiland <weilandss@gmail.com>
2023-07-02 13:56:15 +0800

    template pattern complete

---
+++ C8_TemplatePattern/C8_TemplatePattern/Ducks.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.ObjectModel;
+
+namespace C8_TemplatePattern
+{
+    
+    
+    public class Duck : IComparable<Duck>
+    {
+        private string _name;
+        private int _weight;
+
+        public Duck(string name, int weight)
+        {
+            _name = name;
+            _weight = weight;
+        }
+
+        public override string ToString()
+        {
+            return $"{_name}:{_weight}";
+        }
+        
+        public int CompareTo(Duck otherDuck)
+        {
+            if (_weight < otherDuck._weight) return -1;
+            else if (_weight > otherDuck._weight) return 1;
+            else return 0;
+        }
+
+    }
+
+    public class MyStringList : Collection<string>
+    {
+        private string[] _myList;
+
+        public MyStringList(string[] strings)
+        {
+            _myList = strings;
+        }
+
+        public string Get(int index)
+        {
+            return _myList[index];
+        }
+
+        public int Size() => _myList.Length;
+
+        public string Set(int index, string item)
+        {
+            string oldStr = _myList[index];
+            _myList[index] = item;
+            return oldStr;
+        }
+
+    }
+}
--- C8_TemplatePattern/C8_TemplatePattern/Program.cs
+++ C8_TemplatePattern/C8_TemplatePattern/Program.cs
@@ -6,9 +6,47 @@ namespace C8_TemplatePattern
     {
         public static void Main(string[] args)
         {
+            SortDucks();
+        }
+
+        public static void CoffeeHook()
+        {
             var coffeeHook = new CoffeeWithHook();
             Console.WriteLine($"\nMaking Coffee ...");
             coffeeHook.PrepareRecipe();
+        }
+
+        public static void SortDucks()
+        {
+            Duck[] ducks = new[]
+            {
+                new Duck("Daffy", 8),
+                new Duck("Dewey", 2),
+                new Duck("Howard", 7),
+                new Duck("Louie", 2),
+                new Duck("Donald", 10),
+                new Duck("Huey", 2)
+            };
+            Console.WriteLine($"\nBefore sorting");
+            foreach (var duck in ducks)
+            {
+                Console.WriteLine($"{duck}");
+            }
+            
+            Array.Sort(ducks);
+            
+            Console.WriteLine($"\nAfter sorting");
+            foreach (var duck in ducks)
+            {
+                Console.WriteLine($"{duck}");
+            }
+        }
+
+        public static void TestSublist()
+        {
+            string[] Ducks = { "Mallard Duck", "RedHead Duck", "Rubber Duck", "Decoy Duck" };
+            var ducks = new MyStringList(Ducks);
+            
         }
     }
 }
