83e674c83d0f97a8419c0764e36fa64e955b4878
Weiland <weilandss@gmail.com>
2023-07-04 22:22:20 +0800

    State Pattern complete

--- C10_StatePattern/C10_StatePattern/InterfaceStateMachine.cs
+++ C10_StatePattern/C10_StatePattern/InterfaceStateMachine.cs
@@ -5,7 +5,68 @@ namespace C10_StatePattern.InterfaceSM
 
     public class GumballMachine
     {
-        public void SetState(IState state){}
+        private IState _soldOutState;
+        private IState _noQuarterState;
+        private IState _hasQuarterState;
+        private IState _soldState;
+
+        private IState _state;
+        private int _count = 0;
+
+        public IState SoldOutState => _soldOutState;
+        public IState NoQuarterState => _noQuarterState;
+        public IState HasQuarterState => _hasQuarterState;
+        public IState SoldState => _soldState;
+
+        public int Count => _count;
+
+        public GumballMachine(int numberGumballs)
+        {
+            _soldOutState = new SoldOutState(this);
+            _noQuarterState = new NoQuarterState(this);
+            _hasQuarterState = new HasQuarterState(this);
+            _soldState = new SoldState(this);
+
+            _count = numberGumballs;
+            _state = numberGumballs > 0 ? _noQuarterState : _soldState;
+
+        }
+
+        public void InsertQuarter()
+        {
+            _state.InsertQuarter();
+        }
+
+        public void EjectQuarter()
+        {
+            _state.EjectQuarter();
+        }
+
+        public void TurnCrank()
+        {
+            _state.TurnCrank();
+            // check the state before dispense
+            if (_state == SoldState) _state.Dispense();
+        }
+
+
+        public void SetState(IState state)
+        {
+            _state = state;
+        }
+
+        public void ReleaseBall()
+        {
+            Console.WriteLine($"A gumball comes rolling out of the slot ...");
+            if (_count > 0) _count -= 1;
+        }
+
+        public override string ToString()
+        {
+            return $"Machine with {_count} gumballs, state is {_state.GetType().Name}";
+
+        }
+        
     }
     
     
@@ -31,10 +92,126 @@ namespace C10_StatePattern.InterfaceSM
         public void InsertQuarter()
         {
             Console.WriteLine($"You Inserted a Quarter");
-            _machine.SetState();
+            _machine.SetState(_machine.HasQuarterState);
         }
-        public void EjectQuarter();
-        public void TurnCrank();
-        public void Dispense();
+
+        public void EjectQuarter()
+        {
+            Console.WriteLine($"You have not insert any Quarter");
+        }
+
+        public void TurnCrank()
+        {
+            Console.WriteLine($"You turned, but no quarter");
+        }
+
+        public void Dispense()
+        {
+            Console.WriteLine($"Quarter is needed for dispense");
+        }
     }
+    
+    
+    public class HasQuarterState : IState
+    {
+        private GumballMachine _machine;
+
+        public HasQuarterState(GumballMachine machine)
+        {
+            _machine = machine;
+        }
+
+        public void InsertQuarter()
+        {
+            Console.WriteLine($"You cannot inserted another quarter");
+        }
+
+        public void EjectQuarter()
+        {
+            Console.WriteLine($"Quarter returned");
+            _machine.SetState(_machine.NoQuarterState);
+        }
+
+        public void TurnCrank()
+        {
+            Console.WriteLine($"You turned");
+            _machine.SetState(_machine.SoldState);
+        }
+
+        public void Dispense()
+        {
+            Console.WriteLine($"No gumball dispensed");
+        }
+    }
+    
+    public class SoldState : IState
+    {
+        private GumballMachine _machine;
+
+        public SoldState(GumballMachine machine)
+        {
+            _machine = machine;
+        }
+
+        public void InsertQuarter()
+        {
+            Console.WriteLine($"Gumball on the way, please wait ... ");
+        }
+
+        public void EjectQuarter()
+        {
+            Console.WriteLine($"No quarter to return");
+            _machine.SetState(_machine.NoQuarterState);
+        }
+
+        public void TurnCrank()
+        {
+            Console.WriteLine($"You turned, but nothing happens");
+            _machine.SetState(_machine.SoldState);
+        }
+
+        public void Dispense()
+        {
+            _machine.ReleaseBall();
+            if(_machine.Count>0) _machine.SetState(_machine.NoQuarterState);
+            else
+            {
+                Console.WriteLine($"Out of gumballs");
+                _machine.SetState(_machine.SoldOutState);
+            }
+            
+        }
+    }
+    
+    public class SoldOutState : IState
+    {
+        private GumballMachine _machine;
+
+        public SoldOutState(GumballMachine machine)
+        {
+            _machine = machine;
+        }
+
+        public void InsertQuarter()
+        {
+            Console.WriteLine($"Sold out, not accepting quarter");
+        }
+
+        public void EjectQuarter()
+        {
+            Console.WriteLine($"No quarter to return");
+        }
+
+        public void TurnCrank()
+        {
+            Console.WriteLine($"You turned, but nothing happens");
+        }
+
+        public void Dispense()
+        {
+            Console.WriteLine($"Nothing to dispense");
+        }
+    }
+    
+    
 }
--- C10_StatePattern/C10_StatePattern/Program.cs
+++ C10_StatePattern/C10_StatePattern/Program.cs
@@ -1,9 +1,29 @@
-﻿namespace C10_StatePattern
+﻿using System;
+using C10_StatePattern.InterfaceSM;
+
+namespace C10_StatePattern
 {
     internal class Program
     {
         public static void Main(string[] args)
         {
+            InterfaceSMTest();
+        }
+
+        public static void InterfaceSMTest()
+        {
+            GumballMachine fsm = new GumballMachine(5);
+            Console.WriteLine($"{fsm}");
+            fsm.InsertQuarter();
+            Console.WriteLine($"{fsm}");
+            fsm.TurnCrank();
+            
+            
+            fsm.InsertQuarter();
+            fsm.TurnCrank();
+            fsm.InsertQuarter();
+            fsm.TurnCrank();
+            Console.WriteLine($"{fsm}");
         }
     }
 }
